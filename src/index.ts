b7ffa8dcdad75f0bae8dc49fdab56bb1456deceb
Tyler Long <tyler4long@gmail.com>
2021-03-22 14:37:09 -0700

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,105 @@
-console.log('Try npm run lint/fix!');
-
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
-
-const trailing = 'Semicolon';
-
-const why = 'am I tabbed?';
-
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
+function skip(days: number[], daysToSkip: number) {
+  for (let i = 1; i < days.length; i++) {
+    if (days[i] - days[0] >= daysToSkip) {
+      return days.slice(i);
+    }
   }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
+  return [];
 }
-// TODO: more examples
+
+const cache = new Map();
+function mincostTickets(
+  days: number[],
+  costs: number[],
+  initial = true
+): number {
+  if (days.length === 0) {
+    return 0;
+  }
+  if (initial) {
+    cache.clear();
+  }
+  if (cache.has(days[0])) {
+    return cache.get(days[0]);
+  }
+  const result = Math.min(
+    costs[0] + mincostTickets(skip(days, 1), costs, false),
+    costs[1] + mincostTickets(skip(days, 7), costs, false),
+    costs[2] + mincostTickets(skip(days, 30), costs, false)
+  );
+  cache.set(days[0], result);
+  return result;
+}
+
+console.log(
+  // mincostTickets([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31], [2, 7, 15])
+  mincostTickets(
+    [
+      3,
+      5,
+      6,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      20,
+      21,
+      23,
+      25,
+      26,
+      27,
+      29,
+      30,
+      33,
+      34,
+      35,
+      36,
+      38,
+      39,
+      40,
+      42,
+      45,
+      46,
+      47,
+      48,
+      49,
+      51,
+      53,
+      54,
+      56,
+      57,
+      58,
+      59,
+      60,
+      61,
+      63,
+      64,
+      67,
+      68,
+      69,
+      70,
+      72,
+      74,
+      77,
+      78,
+      79,
+      80,
+      81,
+      82,
+      83,
+      84,
+      85,
+      88,
+      91,
+      92,
+      93,
+      96,
+    ],
+    [3, 17, 57]
+  )
+);
